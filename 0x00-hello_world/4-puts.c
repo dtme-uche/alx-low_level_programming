269cca4e4453e9038e6390e077625e2fbf671035
dtme-uche <dtme.uche@yahoo.com>
2022-09-08 16:42:13 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /* Our name is oyinkansoye susan Adekunle */
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
