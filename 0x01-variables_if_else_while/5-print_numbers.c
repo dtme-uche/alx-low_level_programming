cc86c8dd1c746000de5b749cbba71a33ad3c1718
dtme-uche <dtme.uche@yahoo.com>
2022-09-09 14:45:25 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lower letter
+ * Return: Always 0 (Success)
+ * function main - print alphabet in lower letter
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+printf(n);
+}
