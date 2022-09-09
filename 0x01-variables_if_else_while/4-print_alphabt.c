47fdd88ea9b24c35775351a9bffaa4c9fa69895b
dtme-uche <dtme.uche@yahoo.com>
2022-09-09 14:28:30 -0700

    alpabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lower letter
+ * Return: Always 0 (Success)
+ * function main - print alphabet in lower letter
+ */
+int main(void)
+{
+	char low omit1 omit2;
+	omit1 = 'q';
+	omit2 = 'e';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != omit1 && low != omit2)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
