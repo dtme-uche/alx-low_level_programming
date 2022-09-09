b3bba53c18dc42a99707f2e5470b9e04d0520772
dtme-uche <dtme.uche@yahoo.com>
2022-09-09 12:11:36 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,14 +17,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lstn = n % 10;
-	
+
 	if (lstn > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, lstn);
-	}	else if (lstn == 0)
+	}
+	else if (lstn == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, lstn);
-	}	else if (lstn < 6 && lstn != 0)
+	}
+	else if (lstn < 6 && lstn != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstn);
 	}
