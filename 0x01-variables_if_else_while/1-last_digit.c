1d5c8b043646e5ffd63b491ca5f2136661c2fee8
dtme-uche <dtme.uche@yahoo.com>
2022-09-09 11:57:06 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
     }	else if (lstn == 0)
 	{
 	printf("Last digit of %d is %d and is zero\n", n, lstn);
-    }	else if (lstn < 6 && != 0)
+    }	else if (lstn < 6 && lstn != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstn);
     }
