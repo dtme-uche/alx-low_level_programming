08747579af1e6d80bc17540dd151aa8decb9f303
dtme-uche <dtme.uche@yahoo.com>
2022-09-26 18:13:05 -0700

    me

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
- * Return: *s
+ * _memset - fills memory with a constant byte
+ *
+ * @s: pointer to string
+ * @b: constant byte
+ * @n: first bytes to change
+ * Return: pointer to modified string
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
 	return (s);
 }
