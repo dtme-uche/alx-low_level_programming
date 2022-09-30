8cf15202b83439cbc0d04f14d273151083af83f3
dtme-uche <dtme.uche@yahoo.com>
2022-09-30 12:49:04 -0700

    name of Prog

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the program
+ *
+ * argc: argument count / number of command line arguments.
+ * argv: argument vector / array that contains the program command line arguments.
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
